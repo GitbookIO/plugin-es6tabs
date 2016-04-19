e9b41a2ec95b8bed5b6c2114785fcec19810054e
Samy Pesse <samypesse@gmail.com>
2016-04-20 00:10:43 +0200

    Base to replace all js code blocks

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var babel = require('babel-core');
+var cheerio = require('cheerio');
+
+/*
+    Return list of presets from a config
+*/
+function getPresets() {
+    var presets = this.config.get('pluginsConfig.es6tabs.presets');
+    return presets.map(function(preset) {
+        return require('babel-preset-' + preset);
+    });
+}
+
 
 function es6toEs5(block) {
     var result = babel.transform(block.body, {
-        presets: [
-            require('babel-preset-es2015'),
-        ]
+        presets: getPresets.call(this)
     });
 
     return this.template.applyBlock('codetabs', {
@@ -25,11 +35,51 @@ function es6toEs5(block) {
     });
 }
 
+function replaceCodeBlocks(page) {
+    var that = this;
+    var $ = cheerio.load(page.content);
+
+    $('code.lang-js').each(function() {
+        var $this = $(this);
+        var jsCode = $(this).text();
+        var $pre = $this.parent('pre');
+
+        try {
+            var html = es6toEs5.call(that, {
+                body: jsCode
+            }).body;
+            $pre.replaceWith(html);
+
+            // Reapply highlight on all code blocks
+            $pre.find('code').each(function() {
+                $(this).html(
+                    that.template.applyBlock('code', {
+                        body: $(this).text(),
+                        kwargs: {
+                            language: 'javascript'
+                        }
+                    }).body
+                );
+            });
+        } catch (e) {
+            that.log.error.ln('Error with Javascript code block in "' + page.path + '":');
+            that.log.error.ln(e.stack);
+        }
+    })
+
+
+    page.content = $.html();
+    return page;
+}
+
 module.exports = {
     blocks: {
         es6: {
             process: es6toEs5
         }
+    },
+    hooks: {
+        page: replaceCodeBlocks
     }
 };
 
